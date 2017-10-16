82d97c079600290174df7b4fe064c8acc2ceaf89
Laurence <ljwoodward.code@gmail.com>
2017-10-16 18:30:07 +0100

    added .erb files

--- controller.rb
+++ controller.rb
@@ -4,5 +4,6 @@ require_relative('models/rps.rb')
 
 get '/play/:rps1/:rps2' do
   rock_paper_scissors = Rps.new(params[:rps1], params[:rps2])
-  return rock_paper_scissors.play()
+  @outcome = rock_paper_scissors.play()
+  erb(:result)
 end
